6c718e939015f2930f5153e49686e5a837a47144
tienminh <tienminhdn97@gmail.com>
2022-02-27 22:10:51 +0700

    Init project

---
+++ MyProject/View/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MyProject
+//
+//  Created by T.Minh on 2/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
